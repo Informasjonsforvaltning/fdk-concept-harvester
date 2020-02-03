68f296601dd81812c13bad1d64120cff2b261f65
German Poljakov <gerpol17@gmail.com>
2020-02-03 13:00:25 +0100

    fix: #2489

--- src/main/java/no/ccat/service/ConceptDenormalizedRepository.java
+++ src/main/java/no/ccat/service/ConceptDenormalizedRepository.java
@@ -14,7 +14,4 @@ public interface ConceptDenormalizedRepository
 
     @Query("{\"bool\":{\"must\":[{\"query_string\":{\"default_field\":\"identifier\",\"query\":\"\\\"?0\\\"\"}}]}}")
     List<ConceptDenormalized> findByIdentifier(String identifier);
-
-    @Query("{\"bool\":{\"must\":[{\"query_string\":{\"default_field\":\"identifier\",\"query\":\"\\\"?0\\\"\"}}]}}")
-    List<Object> deleteByIdentifier(String identifier);
 }
