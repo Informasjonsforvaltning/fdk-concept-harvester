b37f3c86bb78e907a992a8466effc4570c9bd70e
NilsOveTen <github.nils.ove@tendenes.com>
2022-03-24 09:11:58 +0100

    chore: use new harvest-admin

--- src/main/kotlin/no/fdk/fdk_concept_harvester/harvester/HarvesterActivity.kt
+++ src/main/kotlin/no/fdk/fdk_concept_harvester/harvester/HarvesterActivity.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Semaphore
 import kotlinx.coroutines.sync.withPermit
 import no.fdk.fdk_concept_harvester.adapter.HarvestAdminAdapter
+import no.fdk.fdk_concept_harvester.model.HarvestAdminParameters
 import no.fdk.fdk_concept_harvester.rabbit.RabbitMQPublisher
 import no.fdk.fdk_concept_harvester.service.UpdateService
 import org.slf4j.LoggerFactory
@@ -32,13 +33,13 @@ class HarvesterActivity(
     @EventListener
     fun fullHarvestOnStartup(event: ApplicationReadyEvent) = initiateHarvest(null)
 
-    fun initiateHarvest(params: Map<String, String>?) {
-        if (params == null || params.isEmpty()) LOGGER.debug("starting harvest of all concept collections")
+    fun initiateHarvest(params: HarvestAdminParameters?) {
+        if (params == null) LOGGER.debug("starting harvest of all concept collections")
         else LOGGER.debug("starting harvest with parameters $params")
 
         val harvest = launch {
             activitySemaphore.withPermit {
-                harvestAdminAdapter.getDataSources(params)
+                harvestAdminAdapter.getDataSources(params?: HarvestAdminParameters())
                     .filter { it.dataType == "concept" }
                     .filter { it.url != null }
                     .forEach {
@@ -59,7 +60,7 @@ class HarvesterActivity(
             LOGGER.debug("Updating union model")
             updateService.updateMetaData()
 
-            if (params.isNullOrEmpty()) LOGGER.debug("completed harvest of all collections")
+            if (params == null) LOGGER.debug("completed harvest of all collections")
             else LOGGER.debug("completed harvest with parameters $params")
 
             publisher.send(HARVEST_ALL_ID)
